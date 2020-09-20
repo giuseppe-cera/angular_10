33010f347cdc91623c21b86223e5f5634f6764fd
giuseppe cera <giuseppe.cera@giuseppes-Mini.homenet.telecomitalia.it>
2020-09-20 20:30:25 +0200

    43

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,19 +2,36 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-servers]',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server> 
-    <app-server></app-server>
-  `, 
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
-    console.log('prova');
+
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created is ' + this.serverName;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName =  (<HTMLInputElement>event.target).value;
   }
 
 }
