c64ceab0f089e9922f7797fefa46f5f0942298fa
giuseppe cera <giuseppe.cera@giuseppes-Mini.homenet.telecomitalia.it>
2020-11-21 08:47:45 +0100

    master

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,34 @@
-import { Directive, ElementRef, OnInit, Renderer2, RendererStyleFlags2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
+  @Input()
+  defaultColor: string = 'transparent';
+
+  @Input('appBetterHighlight')
+  highlightColor: string = 'blue';
+
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
+    this.backgroundColor = this.defaultColor;
   }
 
 }
---
+++ directives-start/src/app/unless.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+  @Input() set appUnless(condition: boolean) {
+    if(!condition) {
+      this.vsRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vsRef.clear();
+    }
+  };
+
+  constructor(private templateRef: TemplateRef<any>, private vsRef: ViewContainerRef) {
+
+   }
+
+}
